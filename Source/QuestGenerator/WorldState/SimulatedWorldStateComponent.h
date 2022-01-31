99dd034f263b8c0a91b64f9227f3255e09708036
Jan Plähn <janplaehn@yahoo.de>
2022-01-31 11:35:36 +0100

    Added Items to World State for Characters

--- Source/QuestGenerator/WorldState/SimulatedWorldStateComponent.h
+++ Source/QuestGenerator/WorldState/SimulatedWorldStateComponent.h
@@ -3,24 +3,62 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "WorldStateProvider.h"
 #include "Components/ActorComponent.h"
+#include "GameFramework/GameModeBase.h"
+
 #include "SimulatedWorldStateComponent.generated.h"
+
+/**
+* Container for simulating npc inventory
+*/
+USTRUCT(BlueprintType)
+struct QUESTGENERATOR_API FCharacterInventory
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FName> Items;
+};
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class QUESTGENERATOR_API USimulatedWorldStateComponent : public UActorComponent
 {
 	GENERATED_BODY()
+public:
+	static USimulatedWorldStateComponent* TryFindWorldStateComponent(const UObject* Context)
+	{
+		const UWorld* World = Context->GetWorld();
+		if (!ensureMsgf(IsValid(World), TEXT("USimulatedWorldStateComponent*: Could not find valid world")))
+		{
+			return nullptr;
+		}
+	
+		AGameModeBase* GameMode =  World->GetAuthGameMode();
+		if (!ensureMsgf(IsValid(GameMode), TEXT("USimulatedWorldStateComponent*: Could not find valid gamemode")))
+		{
+			return nullptr;
+		}
 
-protected:
+		if (!ensureMsgf(GameMode->Implements<UWorldStateProvider>(), TEXT("Could not find IWorldStateProvider Interface on gamemode")))
+		{
+			return nullptr;
+		}	
+		USimulatedWorldStateComponent* WorldStateComponent =  IWorldStateProvider::Execute_GetWorldStateComponent(GameMode);
+		if (!ensureMsgf(IsValid(WorldStateComponent), TEXT("QuestProviderComponent: Could not find quest data creator on gamemode")))
+		{
+			return nullptr;
+		}
+		return WorldStateComponent;
+	}
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TMap<FName, bool> IsCharacterAliveMap;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TMap<FName, bool> PlayerHasItemMap;
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TMap<FName, bool> PlayerHasUsedItemMap;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TMap<FName, bool> PlayerWasDetectedByCharacterMap;
+	TMap<FName, FCharacterInventory> CharacterHasItemMap;
 };
---
+++ Source/QuestGenerator/WorldState/WorldStateProvider.h
@@ -0,0 +1,27 @@
+﻿// Copyright 2022 Jan Plähn. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "WorldStateProvider.generated.h"
+
+class USimulatedWorldStateComponent;
+
+UINTERFACE(MinimalAPI)
+class UWorldStateProvider : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class QUESTGENERATOR_API IWorldStateProvider
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	USimulatedWorldStateComponent* GetWorldStateComponent() const;
+};
